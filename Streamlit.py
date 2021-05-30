74d3f6bd7a131feef838d09ba6e277183f598fed
ubuendia001 <78685864+ubuendia001@users.noreply.github.com>
2021-05-30 13:27:16 +0200

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 st.title('Título de prueba')
 st.write('Prueba')
 df=pd.read_excel('MIBGAS.xlsx',sheet_name='Mensual',index_col=0)
+st.table(df)
+
 indices=['Indice MIBGAS [EUR/MWh]', 'Indice MIBGAS-LNG [EUR/MWh]',
        'Volumen MIBGAS [MWh]', 'Volumen MIBGAS-LNG [MWh]']
 
 indice_select=st.selectbox('Índices',indices)
 
 figura=plt.figure()
-plt.plot(df[indice_select])
+plt.plot(df.loc[:,indice_select])
 
-st.table(df)
+
 st.pyplot(fig=figura)
